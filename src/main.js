9e639b0f44703bc10884048182963c8d79671473
Maarij-Camroodien <maarijcamroodien18@gmail.com>
2023-07-05 12:01:22 +0200

    education

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/style.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { MotionPlugin } from '@vueuse/motion'
 
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(MotionPlugin).mount('#app')
